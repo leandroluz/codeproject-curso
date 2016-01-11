d6e66f9b84c5b75e26886e56f2300ca059f00bbd
Leandro Luz <leandroferreiraluz@gmail.com>
2016-01-11 02:23:20 -0200

    corrigindo exercicios

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        \CodeProject\Client::truncate();
+        factory(\CodeProject\User::class, 10)->create();
+    }
+}
